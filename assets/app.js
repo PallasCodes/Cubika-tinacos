6029a94a89f28742866605319173a7e3c5308821
pallascodes <pallascodes@gmail.com>
2021-08-06 18:24:51 -0500

    prototype v1.0

--- assets/app.js
+++ assets/app.js
@@ -4,3 +4,14 @@ const menu = document.querySelector(".mobile-menu");
 btn.addEventListener("click", () => {
   menu.classList.toggle("hidden");
 });
+
+
+const header = document.getElementById('header')
+
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 220) {
+    header.classList.add('shadow-md')
+  } else {
+    header.classList.remove('shadow-md')
+  }
+})
